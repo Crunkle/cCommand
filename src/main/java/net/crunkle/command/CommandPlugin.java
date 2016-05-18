7259b862052ed1880abcbbf8fbadc6e339eb0234
Crunkle <Crunkle@users.noreply.github.com>
2016-05-19 00:25:32 +0100

    Remove retarded code

--- src/main/java/net/crunkle/command/CommandPlugin.java
+++ src/main/java/net/crunkle/command/CommandPlugin.java
@@ -1,25 +1,10 @@
 package net.crunkle.command;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 
 /**
  * @author Jared Tiala
  */
-public class CommandPlugin extends JavaPlugin
-        implements Listener {
-    @Override
-    public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 0), true);
-    }
+public class CommandPlugin extends JavaPlugin {
+    ;
 }
